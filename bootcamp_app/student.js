6f80d7b5a401248f992ada86b927530d5cf94e85
Roylu7384 <roylu7384@gmail.com>
2022-03-09 21:53:04 -0500

    Modify student.js and teacher.js to use parameterized query

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -1,7 +1,14 @@
 const { Pool } = require('pg');
-const arg = process.argv.slice(2);
-const resultLimit = arg[1];
-const cohortName = arg[0] + '%';
+const cohortName = process.argv[2];
+const resultLimit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, resultLimit];
+const queryString = `
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohort_id=cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -14,13 +21,7 @@ pool.connect((err, res) => {
   console.log('Connected');
 });
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohort_id=cohorts.id
-WHERE cohorts.name LIKE '${cohortName}'
-LIMIT ${resultLimit};
-`)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -7,15 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const value = [`${process.argv[2]}%`];
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id=teachers.id
 JOIN students ON students.id=student_id
 JOIN cohorts ON students.cohort_id=cohorts.id
-WHERE cohorts.name = '${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`).then(res => {
+`;
+
+pool.query(queryString, value).then(res => {
   res.rows.forEach(row => console.log(`${row.cohort}: ${row.teacher}`));
 }).catch(err => {
   console.error('query error', err.stack);
